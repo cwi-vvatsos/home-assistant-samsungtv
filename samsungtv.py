6c9b62d3248beba2290cf70defcede64c952327c
cwi-vvatsos <vvatsos@cwi.gr>
2019-01-26 20:54:58 +0200

    Update samsungtv.py
    Display channel name + DTV program (if available) as media_title

--- samsungtv.py
+++ samsungtv.py
@@ -1,17 +1,21 @@
 """
 Support for interface with an Samsung TV.
 
-For more details about this platform, please refer to the documentation at
+For moee details about this platform, please refer to the documentation at
 https://home-assistant.io/components/media_player.samsungtv/
 """
 import logging
 import socket
 
-REQUIREMENTS = ['wakeonlan==0.2.2', 'beautifulsoup4==4.6.0']
+REQUIREMENTS = ['wakeonlan==0.2.2', 'beautifulsoup4==4.6.0', 'requests==2.21.0']
 
 from bs4 import BeautifulSoup
 
 import voluptuous as vol
+import re
+import requests
+from struct import unpack
+
 
 from homeassistant.components.media_player import (
     SUPPORT_SELECT_SOURCE, SUPPORT_TURN_OFF, SUPPORT_VOLUME_MUTE, SUPPORT_VOLUME_SET,
@@ -90,6 +94,7 @@ class SamsungTVDevice(MediaPlayerDevice):
         self._name = name
         self._mac = mac
         self._wol = wol
+        self._updateCounter = 60
         # Assume that the TV is not muted
         self._muted = False
         self._volume = 0
@@ -104,11 +109,15 @@ class SamsungTVDevice(MediaPlayerDevice):
             'timeout': timeout,
         }
         self._selected_source = ''
+        self._currentChannel = None
+        self._channelsProgram = {}
+        self._channels = {}
         self._source_ids = self.SendSOAP('smp_4_', 'urn:samsung.com:service:MainTVAgent2:1', 'GetSourceList', '', 'id')
         if self._source_ids:
             del self._source_ids[0]
             self._source_names = self.SendSOAP('smp_4_', 'urn:samsung.com:service:MainTVAgent2:1', 'GetSourceList', '', 'sourcetype')
             self._sources = dict(zip(self._source_names, self._source_ids))
+            self.getChannelList()
         else:
             self._source_names = {}
             self._source_ids = {}
@@ -127,12 +136,19 @@ class SamsungTVDevice(MediaPlayerDevice):
             source = self.SendSOAP('smp_4_', 'urn:samsung.com:service:MainTVAgent2:1', 'GetCurrentExternalSource', '','currentexternalsource')
             self._selected_source = source
             self._state = STATE_ON
+            self._updateCounter += 1
+            if self._updateCounter > 10:
+                self.getChannelListProgram()
+                self._updateCounter = 0
+            if source == 'TV':
+                self.getCurrentChannel()
             return True
         else:
             self._state = STATE_OFF
+            self._updateCounter = 0
             return False
 
-    def SendSOAP(self,path,urn,service,body,XMLTag):
+    def SendSOAP(self,path,urn,service,body,XMLTag, regexMatch=False):
         CRLF = "\r\n"
         xmlBody = "";
         xmlBody += '<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">'
@@ -156,7 +172,7 @@ class SamsungTVDevice(MediaPlayerDevice):
         client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         dataBuffer = ''
         response_xml = ''
-        _LOGGER.info("Samsung TV sending: %s", soapRequest)
+#        _LOGGER.info("Samsung TV sending: %s", soapRequest)
 
         try:
             client.connect( (self._config['host'], self._config['port']) )
@@ -173,10 +189,17 @@ class SamsungTVDevice(MediaPlayerDevice):
         response_xml = response_xml.replace("&lt;","<")
         response_xml = response_xml.replace("&gt;",">")
         response_xml = response_xml.replace("&quot;","\"")
-        _LOGGER.info("Samsung TV received: %s", response_xml)
-        if XMLTag:
+#        _LOGGER.info("Samsung TV received: %s", response_xml)
+        if regexMatch:
+            _ma = re.search('.*<'+XMLTag+'>(.*?)</'+XMLTag+'>.*',response_xml)
+            if _ma is not None:
+                return _ma.group(1)
+            else:
+                return ''
+        elif XMLTag:
             soup = BeautifulSoup(str(response_xml), 'html.parser')
             xmlValues = soup.find_all(XMLTag)
+#            _LOGGER.info('SOMETHING: %s',xmlValues)
             xmlValues_names = [xmlValue.string for xmlValue in xmlValues]
             if len(xmlValues_names)== 1:
                 return xmlValues_names[0]
@@ -184,6 +207,83 @@ class SamsungTVDevice(MediaPlayerDevice):
                 return xmlValues_names
         else:
             return response_xml[response_xml.find('<s:Envelope'):]
+
+    def findTag(self, soup, tag):
+        xmlValues = soup.find_all(tag)
+        xmlValues_names = [xmlValue.string for xmlValue in xmlValues]
+        if len(xmlValues_names)== 1:
+            return xmlValues_names[0]
+        else:
+            return xmlValues_names
+
+    def getChannelList(self):
+#        _LOGGER.info('getting channel list')
+        _uri = self.SendSOAP('smp_4_', 'urn:samsung.com:service:MainTVAgent2:1', 'GetChannelListURL', '','channellisturl')
+#        _LOGGER.info('got uri: %s',_uri)
+        if _uri is not None:
+            r = requests.get(_uri)
+        self._parse_channel_list(r.content)
+
+    def _getint(self, buf, offset):
+        """Helper function to extract a 16-bit little-endian unsigned from a char
+        buffer 'buf' at offset 'offset'..'offset'+2."""
+        x = unpack('<H', buf[offset:offset+2])
+        return x[0]
+
+    def getChannelListProgram(self):
+        _LOGGER.info('getting channel list')
+        _uri = self.SendSOAP('smp_4_', 'urn:samsung.com:service:MainTVAgent2:1', 'GetCurrentProgramInformationURL', '','currentproginfourl')
+#        _LOGGER.info('got uri: %s',_uri)
+        if _uri is not None:
+            r = requests.get(_uri)
+#            _content = bytes(r.content, 'utf-8')
+            _content = r.content.decode(encoding="utf-8")
+            _program = BeautifulSoup(_content, 'html.parser')
+            _channelIDs = self.findTag(_program, 'majorch')
+            _mediaTitles = self.findTag(_program, 'title')
+            self._channelsProgram = {}
+            for i in range(len(_channelIDs)):
+                self._channelsProgram[_channelIDs[i]]= {'title': _mediaTitles[i]}
+            return self._channelsProgram
+        else:
+            return {}
+
+
+    def getCurrentChannel(self):
+        _ch = self.SendSOAP('smp_4_', 'urn:samsung.com:service:MainTVAgent2:1', 'GetCurrentMainTVChannel', '','majorch')
+        if _ch in self._channels:
+            self._currentChannel = self._channels[_ch]
+#            _LOGGER.info('CurrentChannel: %s',self._currentChannel)
+        else:
+            self._currentChannel = None
+#            _LOGGER.info('Channel %s not found in channel list. Using %s',_ch,self._currentChannel)
+
+    def _parse_channel_list(self, channel_list):
+        """Splits the binary channel list into channel entry fields and returns a list of Channels."""
+
+        # The channel list is binary file with a 4-byte header, containing 2 unknown bytes and
+        # 2 bytes for the channel count, which must be len(list)-4/124, as each following channel
+        # is 124 bytes each. See Channel._parse_dat for how each entry is constructed.
+
+        if len(channel_list) < 128:
+            _LOGGER.info('channel list is smaller than it has to be for at least one channel (%d bytes (actual) vs. 128 bytes',len(channel_list))
+
+        if (len(channel_list)-4) % 124 != 0:
+            _LOGGER.info('channel list\'s size (%d) minus 128 (header) is not a multiple of 124 bytes',len(channel_list))
+
+        actual_channel_list_len = (len(channel_list)-4) / 124
+        expected_channel_list_len = self._getint(channel_list, 2)
+        if actual_channel_list_len != expected_channel_list_len:
+            _LOGGER.info('Actual channel list length ((%d-4)/124=%d) does not equal expected channel list length (%d) as defined in header',len(channel_list),actual_channel_list_len, expected_channel_list_len)
+
+        self._channels = {}
+        pos = 4
+        while pos < len(channel_list):
+            chunk = channel_list[pos:pos+124]
+            _channel = Channel(chunk)
+            self._channels['{}'.format(_channel.major_ch)] = _channel
+            pos += 124
+        return self._channels
 
     @property
     def name(self):
@@ -218,7 +318,15 @@ class SamsungTVDevice(MediaPlayerDevice):
     @property
     def media_title(self):
         """Title of current playing media."""
-        return self._selected_source
+        if self._currentChannel is not None:
+            _chName = self._currentChannel.title
+        else:
+            return 'Unknown'
+        _ch = '{}'.format(self._currentChannel.major_ch)
+        if _ch in self._channelsProgram:
+            return _chName+' - '+str(self._channelsProgram[_ch]['title'])
+        else:
+            return _chName+' - Unknown'
 
     @property
     def supported_features(self):
@@ -261,3 +369,78 @@ class SamsungTVDevice(MediaPlayerDevice):
         """Turn the media player on."""
         if self._mac:
             self._wol.send_magic_packet(self._mac)
+
+
+
+class Channel(object):
+    """Class representing a Channel from the TV's channel list."""
+
+    def __init__(self, from_dat):
+        """Constructs the Channel object from a binary channel list chunk."""
+        self._parse_dat(from_dat)
+
+    def _parse_dat(self, buf):
+        """Parses the binary data from a channel list chunk and initilizes the
+        member variables."""
+
+        # Each entry consists of (all integers are 16-bit little-endian unsigned):
+        #   [2 bytes int] Type of the channel. I've only seen 3 and 4, meaning
+        #                 CDTV (Cable Digital TV, I guess) or CATV (Cable Analog
+        #                 TV) respectively as argument for <ChType>
+        #   [2 bytes int] Major channel (<MajorCh>)
+        #   [2 bytes int] Minor channel (<MinorCh>)
+        #   [2 bytes int] PTC (Physical Transmission Channel?), <PTC>
+        #   [2 bytes int] Program Number (in the mux'ed MPEG or so?), <ProgNum>
+        #   [2 bytes int] They've always been 0xffff for me, so I'm just assuming
+        #                 they have to be :)
+        #   [4 bytes string, \0-padded] The (usually 3-digit, for me) channel number
+        #                               that's displayed (and which you can enter), in ASCII
+        #   [2 bytes int] Length of the channel title
+        #   [106 bytes string, \0-padded] The channel title, in UTF-8 (wow)
+
+        t = self._getint(buf, 0)
+        if t == 4:
+            self.ch_type = 'CDTV'
+        elif t == 3:
+            self.ch_type = 'CATV'
+        elif t == 2:
+            self.ch_type = 'DTV'
+        else:
+            _LOGGER.info('Unknown channel type %d', t)
+
+        self.major_ch = self._getint(buf, 2)
+        self.minor_ch = self._getint(buf, 4)
+        self.ptc      = self._getint(buf, 6)
+        self.prog_num = self._getint(buf, 8)
+
+        if self._getint(buf, 10) != 0xffff:
+            _LOGGER.info('reserved field mismatch (%04x)', self._getint(buf, 10))
+
+        self.dispno = buf[12:16].decode('utf-8').rstrip('\x00')
+        title_len = self._getint(buf, 22)
+        self.title = buf[24:24+title_len].decode('utf-8')
+
+    def _getint(self, buf, offset):
+        """Helper function to extract a 16-bit little-endian unsigned from a char
+        buffer 'buf' at offset 'offset'..'offset'+2."""
+        x = unpack('<H', buf[offset:offset+2])
+        return x[0]
+
+    def display_string(self):
+        """Returns a unicode display string, since both __repr__ and __str__ convert it
+        to ascii."""
+
+        return u'[%s] % 4s %s' % (self.ch_type, self.dispno, self.title)
+
+    def __repr__(self):
+        return '<Channel %s %s ChType=%s MajorCh=%d MinorCh=%d PTC=%d ProgNum=%d>' % \
+            (self.dispno, repr(self.title), self.ch_type, self.major_ch, self.minor_ch, self.ptc,
+             self.prog_num)
+
+    @property
+    def as_xml(self):
+        """The channel list as XML representation for SetMainTVChannel."""
+
+        return ('<?xml version="1.0" encoding="UTF-8" ?><Channel><ChType>%s</ChType><MajorCh>%d'
+                '</MajorCh><MinorCh>%d</MinorCh><PTC>%d</PTC><ProgNum>%d</ProgNum></Channel>') % \
+            (escape(self.ch_type), self.major_ch, self.minor_ch, self.ptc, self.prog_num)
